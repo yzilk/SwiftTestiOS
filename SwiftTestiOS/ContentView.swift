c2792ee959dc5f6790dfc1336213c4c004413517
ykzil.mckm <yuzzzn@gmail.com>
2025-03-15 00:37:33 +0900

    Initial Commit

---
+++ SwiftTestiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTestiOS
+//
+//  Created by Yugo Noji on 2025/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTestiOS/SwiftTestiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestiOSApp.swift
+//  SwiftTestiOS
+//
+//  Created by Yugo Noji on 2025/03/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
