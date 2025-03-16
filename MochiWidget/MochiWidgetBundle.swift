d366f31a8ee5cce649abd3b694761db74c5c3106
ykzil.mckm <yuzzzn@gmail.com>
2025-03-17 01:33:10 +0900

    TaskandDateRegistration

---
+++ MochiWidget/MochiWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MochiWidgetBundle.swift
+//  MochiWidget
+//
+//  Created by Yugo Noji on 2025/03/15.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MochiWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MochiWidget()
+    }
+}
